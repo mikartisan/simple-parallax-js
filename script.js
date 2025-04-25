920f56c6553ad162769af38acfc950b658ab7fdf
johnmichael <johnmichael.nspire@gmail.com>
2025-04-25 08:57:47 +0800

    Create own parallax design

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
-let text = document.getElementById("text");
-let leaf = document.getElementById("leaf");
-let hill1 = document.getElementById("hill1");
-let hill4 = document.getElementById("hill4");
-let hill5 = document.getElementById("hill5");
+let hillFrontLeft = document.getElementById("hillFrontLeft");
+let hillFrontRight = document.getElementById("hillFrontRight");
+let water = document.getElementById("water");
+let hillBack = document.getElementById("hillBack");
+let mountainLeft = document.getElementById("mountainLeft");
+let mountainRight = document.getElementById("mountainRight");
+let cloudLeft = document.getElementById("cloudLeft");
+const clouds = [
+    { element: document.getElementById("cloudLeft"), position: 0, speed: 1 },
+    { element: document.getElementById("cloudRight"), position: 0, speed: 0.5 }
+];
+
+const resetPosition = 200; 
+const endPosition = window.innerWidth; 
 
 window.addEventListener("scroll", () => {
     let value = window.scrollY;
+    let maxScroll = 500;
 
-    text.style.marginTop = value * 2.5 + "px";
-    leaf.style.top = value * -1.5 + "px";
-    leaf.style.left = value * 1.5 + "px";
-    hill5.style.left = value * 1.5 + "px";
-    hill4.style.left = value * -1.5 + "px";
-    hill3.style.left = value * 1.5 + "px";
-    hill3.style.top = value * 1.5 + "px";
-    hill2.style.left = value * -1.5 + "px";
-    hill2.style.top = value * 1.5 + "px";
-    hill1.style.top = value * 1.5 + "px";
+    if (value <= maxScroll) {
+        hillFrontLeft.style.top = value * 1.5 + "px";
+        hillFrontLeft.style.left = value * -1.5 + "px";
+
+        hillFrontRight.style.top = value * 1 + "px";
+        hillFrontRight.style.left = value * 1 + "px";
+
+        water.style.top = value * 1 + "px";
+        hillBack.style.top = value * 1 + "px";
+
+        mountainLeft.style.left = value * -1 + "px";
+        mountainLeft.style.top = value * 1 + "px";
+
+        mountainRight.style.left = value * 1 + "px";
+        mountainRight.style.top = value * 1 + "px";
+    }
 });
+
+function animateCloud() {
+    cloudPosition += cloudSpeed;
+    if (cloudPosition > endPosition) {
+        cloudPosition = resetPosition;
+    }
+    cloudLeft.style.left = cloudPosition + "px";
+}
+
+function animateClouds() {
+    clouds.forEach(cloud => {
+        cloud.position += cloud.speed;
+        if (cloud.position > endPosition) {
+        cloud.position = resetPosition;
+        }
+        cloud.element.style.left = cloud.position + "px";
+    });
+}
+
+  setInterval(animateClouds, 50); // Approximately 60 frames per second
